560dbc00f665dfedbd69f1aeafe7ae4a649a614b
BeautyJeong <wjdqbxl@gachon.ac.kr>
2023-12-15 11:23:42 +0900

    #6 [2021 UPDATE] EFFECTS > #6.3 Recap: useEffect & 지금까지 배운 내용 다시 복습 --- - useEffect는 두 개의 argument만 가진다. --> 실행하려는 코드, dependency(리액트가 지켜보는 것)로만 이루어져 있다.   - dependency가 존재하면, 해당 리스트의 값이 변화할 때만 코드가 실행된다. - 코드를 언제 실행할 지 선택하는 useEffect - 처음 한 번만 실행하게 할 수 도 있고, state이 변할 때만 코드를 실행하게 할 수 도 있다(내가 원하는 시점에 코드 실행)
    ## useEffect 왜 써?
    렌더링이 계속 된다면, 특히 특정 api를 불러오게 되는 경우 계속해서 불러오는 문제가 생길 수 있다.
    state를 변경할 때, 계속해서 렌더링 되는 문제점이 존재한다. 많은 state가 존재한다면 성능 저하 문제가 발생할 수 있다. 이런 문제를 해결하기 위해 사용한다.
    
    ## 지금까지 배운 내용 다시 복습
    ---
    - 리액트를 사용하는 이유: 최소 단위의 렌더링을 위해
    - useState(): 변수, 변수를 제어하는 함수로 구성되며 변하는 값을 제어, 해당 부분의 리렌더링을 위함
    - props: 태그의 속성 값을 함수의 아규먼트 처럼 컴포넌트에 값을 전달해준다.
    - useEffect(): 코드의 실행 시점을 관리할 수 있는 선택권을 얻는 방어막 같은 존재, 디펜던시가 없을 경우 최초 1회 실행, 있을 경우 해당 값이 변할 경우 실행한다. 이 때 디펜던시는 여러개 입력이 가능하다.
    - 부모 컴포넌트에서 리렌더링이 일어날 경우 모든 자식들이 리렌더링이 된다.(wa can use memo)
    - propType을 설치하고 props의 타입을 지정해 줄 수 있다. 이 때 isRequired로 필수값을 지정 가능

--- react-for-beginners-app/src/App.js
+++ react-for-beginners-app/src/App.js
@@ -8,21 +8,21 @@ function App() {
 
     useEffect(() => {
         console.log("I run only once.");
-    }, []);
+    }, []); //dependency(배열)에 아무것도 없기 때문에 코드는 처음 한 번만 실행됨
 
     useEffect( () => {
         if (keyword !== "") {
             console.log("I run when 'keyword' changes.");
         }
-    }, [keyword])
+    }, [keyword]) //keyword가 변할 때 코드 실행
 
     useEffect( () => {
         console.log("I run when 'counter' changes.");
-    }, [counter])
+    }, [counter]) //처음, counter가 변할 때 코드 실행
 
     useEffect( () => {
         console.log("I run when 'counter' & 'keyword' change.");
-    }, [counter, keyword] )
+    }, [counter, keyword] ) // 처음, counter와 keyword가 변할 때 코드 실행
 
   return (
       <div>
