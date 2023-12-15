0fd9117f7e65aeabdaa6927438edddc8f0875f07
BeautyJeong <wjdqbxl@gachon.ac.kr>
2023-12-15 11:48:16 +0900

    #6 [2021 UPDATE] EFFECTS > #6.4 Cleanup: useEffect --- - useEffect는 컴포넌트가 destroy될 때도 코드를 실행할 수 있음 - function을 retrun하면 컴포넌트가 사라질 때도 코드 실행 가능

--- react-for-beginners-app/src/App.js
+++ react-for-beginners-app/src/App.js
@@ -1,34 +1,20 @@
 import {useEffect, useState} from "react";
 
-function App() {
-    const [counter, setValue] = useState(0);
-    const [keyword, setKeyword] = useState("");
-    const onChange = (event) => setKeyword(event.target.value);
-    const onClick = () => setValue((prev) => prev + 1);
-
+function Hello() {
     useEffect(() => {
-        console.log("I run only once.");
-    }, []); //dependency(배열)에 아무것도 없기 때문에 코드는 처음 한 번만 실행됨
-
-    useEffect( () => {
-        if (keyword !== "") {
-            console.log("I run when 'keyword' changes.");
-        }
-    }, [keyword]) //keyword가 변할 때 코드 실행
-
-    useEffect( () => {
-        console.log("I run when 'counter' changes.");
-    }, [counter]) //처음, counter가 변할 때 코드 실행
-
-    useEffect( () => {
-        console.log("I run when 'counter' & 'keyword' change.");
-    }, [counter, keyword] ) // 처음, counter와 keyword가 변할 때 코드 실행
+       console.log("created~ :)");
+       return () => console.log("destroy :(") //컴포넌트가 없어질 때 실행
+    }, []);
+    return <h1>Hello</h1>
+}
+function App() {
+    const [showing, setShowing] = useState(false);
+    const onClick = () => setShowing((prev) => !prev);
 
   return (
       <div>
-          <input value={keyword} onChange={onChange} type="text" placeholder="Search here~" />
-          <h1>{counter}</h1>
-          <button onClick={onClick}>Click me!</button>
+          {showing ? <Hello /> : null}
+          <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
       </div>
   );
 }
